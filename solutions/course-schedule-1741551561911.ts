6feaf83c4290a6874eb0dab1568c8c6bf6b6346b
gary-rivera <a.gary.rivera@gmail.com>
2025-03-09 20:19:28 +0000

    Add solution course-schedule-1741551561911.ts

---
+++ solutions/course-schedule-1741551561911.ts
@@ -0,0 +1,66 @@
+function canFinish(numCourses: number, prerequisites: number[][]): boolean {
+    let graph: number[][] = Array(numCourses).fill(0).map(() => []);
+    let visited: number[] = Array(numCourses).fill(0);
+
+    for (let [course, pre] of prerequisites) {
+        graph[course].push(pre);
+    }
+
+    function dfs(course: number): boolean {
+        if (visited[course] === -1) return false;
+        if (visited[course] === 1) return true;
+
+        visited[course] = -1;
+
+        for (let pre of graph[course]) {
+            if (!dfs(pre)) return false;
+        }
+
+        visited[course] = 1;
+        return true;
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) return false;
+    }
+
+    return true;
+}
+
+/*
+question: There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+
+	For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+
+
+Return true if you can finish all courses. Otherwise, return false.
+
+ 
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+
+
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+
+
+ 
+Constraints:
+
+
+	1 <= numCourses <= 2000
+	0 <= prerequisites.length <= 5000
+	prerequisites[i].length == 2
+	0 <= ai, bi < numCourses
+	All the pairs prerequisites[i] are unique.
+
+ */
