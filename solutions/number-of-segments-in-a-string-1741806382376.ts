18a9be9791eeb3b38c07957dcb08ced0595a8324
gary-rivera <a.gary.rivera@gmail.com>
2025-03-12 19:06:25 +0000

    Add solution number-of-segments-in-a-string-1741806382376.ts

---
+++ solutions/number-of-segments-in-a-string-1741806382376.ts
@@ -0,0 +1,32 @@
+function countSegments(s: string): number {
+    return s.split(' ').filter(str => str.length > 0).length;
+}
+
+/*
+question: Given a string s, return the number of segments in the string.
+
+A segment is defined to be a contiguous sequence of non-space characters.
+
+ 
+Example 1:
+
+Input: s = "Hello, my name is John"
+Output: 5
+Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+
+
+Example 2:
+
+Input: s = "Hello"
+Output: 1
+
+
+ 
+Constraints:
+
+
+	0 <= s.length <= 300
+	s consists of lowercase and uppercase English letters, digits, or one of the following characters "!@#$%^&*()_+-=',.:".
+	The only space character in s is ' '.
+
+ */
