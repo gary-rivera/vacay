5b37950b351296cd6c3571b9227659526617b602
gary-rivera <a.gary.rivera@gmail.com>
2025-07-17 23:20:00 +0000

    Add solution remove-linked-list-elements-1752794393035.ts

---
+++ solutions/remove-linked-list-elements-1752794393035.ts
@@ -0,0 +1,54 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    let dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    let current = dummyHead;
+    while (current.next !== null) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    return dummyHead.next;
+}
+
+/*
+question: Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+Example 1:
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+
+
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+
+
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+
+
+ 
+Constraints:
+
+
+	The number of nodes in the list is in the range [0, 104].
+	1 <= Node.val <= 50
+	0 <= val <= 50
+
+ */
