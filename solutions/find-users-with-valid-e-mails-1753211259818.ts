29de5cb15fe3d0a6801b3d080546d6e409d79ff6
gary-rivera <a.gary.rivera@gmail.com>
2025-07-22 19:07:57 +0000

    Add solution find-users-with-valid-e-mails-1753211259818.ts

---
+++ solutions/find-users-with-valid-e-mails-1753211259818.ts
@@ -0,0 +1,98 @@
+class User {
+    user_id: number;
+    name: string;
+    mail: string;
+
+    constructor(user_id: number, name: string, mail: string) {
+        this.user_id = user_id;
+        this.name = name;
+        this.mail = mail;
+    }
+}
+
+function validateEmails(users: User[]): User[] {
+    const validUsers: User[] = [];
+    const emailRegex = /^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode.com$/;
+
+    for (let user of users) {
+        if (emailRegex.test(user.mail)) {
+            validUsers.push(user);
+        }
+    }
+
+    return validUsers;
+}
+
+// Test the function
+const users: User[] = [
+    new User(1, 'Winston', 'winston@leetcode.com'),
+    new User(2, 'Jonathan', 'jonathanisgreat'),
+    new User(3, 'Annabelle', 'bella-@leetcode.com'),
+    new User(4, 'Sally', 'sally.come@leetcode.com'),
+    new User(5, 'Marwan', 'quarz#2020@leetcode.com'),
+    new User(6, 'David', 'david69@gmail.com'),
+    new User(7, 'Shapiro', '.shapo@leetcode.com')
+];
+
+console.log(validateEmails(users));
+
+/*
+question: Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user_id       | int     |
+| name          | varchar |
+| mail          | varchar |
++---------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains information of the users signed up in a website. Some e-mails are invalid.
+
+
+ 
+
+Write a solution to find the users who have valid emails.
+
+A valid e-mail has a prefix name and a domain where:
+
+
+	The prefix name is a string that may contain letters (upper or lower case), digits, underscore '_', period '.', and/or dash '-'. The prefix name must start with a letter.
+	The domain is '@leetcode.com'.
+
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+Example 1:
+
+Input: 
+Users table:
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 2       | Jonathan  | jonathanisgreat         |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
+| 5       | Marwan    | quarz#2020@leetcode.com |
+| 6       | David     | david69@gmail.com       |
+| 7       | Shapiro   | .shapo@leetcode.com     |
++---------+-----------+-------------------------+
+Output: 
++---------+-----------+-------------------------+
+| user_id | name      | mail                    |
++---------+-----------+-------------------------+
+| 1       | Winston   | winston@leetcode.com    |
+| 3       | Annabelle | bella-@leetcode.com     |
+| 4       | Sally     | sally.come@leetcode.com |
++---------+-----------+-------------------------+
+Explanation: 
+The mail of user 2 does not have a domain.
+The mail of user 5 has the # sign which is not allowed.
+The mail of user 6 does not have the leetcode domain.
+The mail of user 7 starts with a period.
+
+ */
