6874c2847bf5f47e4cdbe8776c4ad9f3d9458433
gary-rivera <a.gary.rivera@gmail.com>
2025-08-11 00:46:13 +0000

    Add solution set-mismatch-1754873168839.ts

---
+++ solutions/set-mismatch-1754873168839.ts
@@ -0,0 +1,46 @@
+function findErrorNums(nums: number[]): number[] {
+    let duplicate: number;
+    let missing: number;
+    const numSet = new Set<number>();
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (numSet.has(nums[i])) {
+            duplicate = nums[i];
+        } else {
+            numSet.add(nums[i]);
+        }
+    }
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!numSet.has(i)) {
+            missing = i;
+            break;
+        }
+    }
+
+    return [duplicate, missing];
+}
+
+/*
+question: You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+
+You are given an integer array nums representing the data status of this set after the error.
+
+Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+ 
+Example 1:
+Input: nums = [1,2,2,4]
+Output: [2,3]
+Example 2:
+Input: nums = [1,1]
+Output: [1,2]
+
+ 
+Constraints:
+
+
+	2 <= nums.length <= 104
+	1 <= nums[i] <= 104
+
+ */
