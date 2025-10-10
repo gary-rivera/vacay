10cb86b151be9e6789eb19d6f9bcdb1d623011e8
gary-rivera <a.gary.rivera@gmail.com>
2025-10-10 21:46:57 +0000

    Add solution first-completely-painted-row-or-column-1760132807852.ts

---
+++ solutions/first-completely-painted-row-or-column-1760132807852.ts
@@ -0,0 +1,64 @@
+type Position = { row: number, col: number };
+
+function smallestIndex(arr: number[], mat: number[][]): number {
+    const m = mat.length;
+    const n = mat[0].length;
+    const pos: Position[] = new Array(m * n + 1);
+    const rowPainted: number[] = new Array(m).fill(0);
+    const colPainted: number[] = new Array(n).fill(0);
+
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            pos[mat[i][j]] = { row: i, col: j };
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        const { row, col } = pos[arr[i]];
+        rowPainted[row]++;
+        colPainted[col]++;
+
+        if (rowPainted[row] === n || colPainted[col] === m) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+/*
+question: You are given a 0-indexed integer array arr, and an m x n integer matrix mat. arr and mat both contain all the integers in the range [1, m * n].
+
+Go through each index i in arr starting from index 0 and paint the cell in mat containing the integer arr[i].
+
+Return the smallest index i at which either a row or a column will be completely painted in mat.
+
+ 
+Example 1:
+
+Input: arr = [1,3,4,2], mat = [[1,4],[2,3]]
+Output: 2
+Explanation: The moves are shown in order, and both the first row and second column of the matrix become fully painted at arr[2].
+
+
+Example 2:
+
+Input: arr = [2,8,7,4,1,3,5,6,9], mat = [[3,2,5],[1,4,6],[8,7,9]]
+Output: 3
+Explanation: The second column becomes fully painted at arr[3].
+
+
+ 
+Constraints:
+
+
+	m == mat.length
+	n = mat[i].length
+	arr.length == m * n
+	1 <= m, n <= 105
+	1 <= m * n <= 105
+	1 <= arr[i], mat[r][c] <= m * n
+	All the integers of arr are unique.
+	All the integers of mat are unique.
+
+ */
