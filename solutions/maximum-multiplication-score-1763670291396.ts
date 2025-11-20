4d294ddfd863838146dea417dc41b821090416e6
gary-rivera <a.gary.rivera@gmail.com>
2025-11-20 20:25:02 +0000

    Add solution maximum-multiplication-score-1763670291396.ts

---
+++ solutions/maximum-multiplication-score-1763670291396.ts
@@ -0,0 +1,55 @@
+function maxScore(a: number[], b: number[]): number {
+    let n = b.length;
+    let dp: number[][] = Array(5).fill(0).map(() => Array(n + 1).fill(-1e9));
+    dp[0][0] = 0;
+
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j <= n; j++) {
+            if (j < n) {
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + a[i] * b[j]);
+            }
+            dp[i][j + 1] = Math.max(dp[i][j + 1], dp[i][j]);
+        }
+    }
+    return dp[4][n];
+}
+
+/*
+question: You are given an integer array a of size 4 and another integer array b of size at least 4.
+
+You need to choose 4 indices i0, i1, i2, and i3 from the array b such that i0 < i1 < i2 < i3. Your score will be equal to the value a[0] * b[i0] + a[1] * b[i1] + a[2] * b[i2] + a[3] * b[i3].
+
+Return the maximum score you can achieve.
+
+ 
+Example 1:
+
+
+Input: a = [3,2,5,6], b = [2,-6,4,-5,-3,2,-7]
+
+Output: 26
+
+Explanation:
+We can choose the indices 0, 1, 2, and 5. The score will be 3 * 2 + 2 * (-6) + 5 * 4 + 6 * 2 = 26.
+
+
+Example 2:
+
+
+Input: a = [-1,4,5,-2], b = [-5,-1,-3,-2,-4]
+
+Output: -1
+
+Explanation:
+We can choose the indices 0, 1, 3, and 4. The score will be (-1) * (-5) + 4 * (-1) + 5 * (-2) + (-2) * (-4) = -1.
+
+
+ 
+Constraints:
+
+
+	a.length == 4
+	4 <= b.length <= 105
+	-105 <= a[i], b[i] <= 105
+
+ */
