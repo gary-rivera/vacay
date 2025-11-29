70d23ddda3e4a79caac3b693b0987ad96a3a9c50
gary-rivera <a.gary.rivera@gmail.com>
2025-11-29 21:54:54 +0000

    Add solution count-elements-with-maximum-frequency-1764453288576.ts

---
+++ solutions/count-elements-with-maximum-frequency-1764453288576.ts
@@ -0,0 +1,55 @@
+function maxFrequencyElements(nums: number[]): number {
+    let frequencyMap: { [key: number]: number } = {};
+    let maxFrequency = 0;
+    let count = 0;
+
+    for (let num of nums) {
+        if (frequencyMap[num]) {
+            frequencyMap[num]++;
+        } else {
+            frequencyMap[num] = 1;
+        }
+
+        if (frequencyMap[num] > maxFrequency) {
+            maxFrequency = frequencyMap[num];
+            count = frequencyMap[num];
+        } else if (frequencyMap[num] === maxFrequency) {
+            count += frequencyMap[num];
+        }
+    }
+
+    return count;
+}
+
+/*
+question: You are given an array nums consisting of positive integers.
+
+Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+
+The frequency of an element is the number of occurrences of that element in the array.
+
+ 
+Example 1:
+
+Input: nums = [1,2,2,3,1,4]
+Output: 4
+Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+So the number of elements in the array with maximum frequency is 4.
+
+
+Example 2:
+
+Input: nums = [1,2,3,4,5]
+Output: 5
+Explanation: All elements of the array have a frequency of 1 which is the maximum.
+So the number of elements in the array with maximum frequency is 5.
+
+
+ 
+Constraints:
+
+
+	1 <= nums.length <= 100
+	1 <= nums[i] <= 100
+
+ */
