47c70a3c7f2990cf1042484c32ff90a8e3b799b7
gary-rivera <a.gary.rivera@gmail.com>
2025-12-07 21:55:41 +0000

    Add solution contains-duplicate-ii-1765144535755.ts

---
+++ solutions/contains-duplicate-ii-1765144535755.ts
@@ -0,0 +1,44 @@
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+    let map: Map<number, number> = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        if (map.has(nums[i])) {
+            if (i - map.get(nums[i])! <= k) {
+                return true;
+            }
+        }
+        map.set(nums[i], i);
+    }
+    return false;
+}
+
+/*
+question: Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
+
+ 
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+
+
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+
+
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+
+
+ 
+Constraints:
+
+
+	1 <= nums.length <= 105
+	-109 <= nums[i] <= 109
+	0 <= k <= 105
+
+ */
