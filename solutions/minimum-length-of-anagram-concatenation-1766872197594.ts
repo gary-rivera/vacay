b56e321ead220f6e3c024e30d46b0e49b4c4afe5
gary-rivera <a.gary.rivera@gmail.com>
2025-12-27 21:50:01 +0000

    Add solution minimum-length-of-anagram-concatenation-1766872197594.ts

---
+++ solutions/minimum-length-of-anagram-concatenation-1766872197594.ts
@@ -0,0 +1,56 @@
+function minAnagramLength(s: string): number {
+    let uniqueChars = new Set<string>();
+    for (let i = 0; i < s.length; i++) {
+        uniqueChars.add(s[i]);
+    }
+    return uniqueChars.size;
+}
+
+/*
+question: You are given a string s, which is known to be a concatenation of anagrams of some string t.
+
+Return the minimum possible length of the string t.
+
+An anagram is formed by rearranging the letters of a string. For example, "aab", "aba", and, "baa" are anagrams of "aab".
+
+ 
+Example 1:
+
+
+Input: s = "abba"
+
+Output: 2
+
+Explanation:
+
+One possible string t could be "ba".
+
+
+Example 2:
+
+
+Input: s = "cdef"
+
+Output: 4
+
+Explanation:
+
+One possible string t could be "cdef", notice that t can be equal to s.
+
+
+Example 2:
+
+
+Input: s = "abcbcacabbaccba"
+
+Output: 3
+
+
+ 
+Constraints:
+
+
+	1 <= s.length <= 105
+	s consist only of lowercase English letters.
+
+ */
