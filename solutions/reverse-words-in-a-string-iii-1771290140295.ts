a4414a824c1c7a8babd1365cadda10947ba115f6
gary-rivera <a.gary.rivera@gmail.com>
2026-02-17 01:02:23 +0000

    Add solution reverse-words-in-a-string-iii-1771290140295.ts

---
+++ solutions/reverse-words-in-a-string-iii-1771290140295.ts
@@ -0,0 +1,31 @@
+function reverseWords(s: string): string {
+    return s.split(' ').map(word => word.split('').reverse().join('')).join(' ');
+}
+
+/*
+question: Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+
+
+Example 2:
+
+Input: s = "Mr Ding"
+Output: "rM gniD"
+
+
+ 
+Constraints:
+
+
+	1 <= s.length <= 5 * 104
+	s contains printable ASCII characters.
+	s does not contain any leading or trailing spaces.
+	There is at least one word in s.
+	All the words in s are separated by a single space.
+
+ */
