d03029eaeededd16a7a74ee51dfbb6125a64a414
gary-rivera <a.gary.rivera@gmail.com>
2026-03-24 00:54:22 +0000

    Add solution customers-who-never-order-1774313655132.ts

---
+++ solutions/customers-who-never-order-1774313655132.ts
@@ -0,0 +1,81 @@
+type Customer = {
+    id: number;
+    name: string;
+}
+
+type Order = {
+    id: number;
+    customerId: number;
+}
+
+function findCustomersWhoNeverOrder(customers: Customer[], orders: Order[]): string[] {
+    let customerIdsWithOrders = new Set(orders.map(order => order.customerId));
+    let customersWhoNeverOrder = customers.filter(customer => !customerIdsWithOrders.has(customer.id));
+    return customersWhoNeverOrder.map(customer => customer.name);
+}
+
+/*
+question: Table: Customers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+
+
+ 
+
+Table: Orders
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+
+
+ 
+
+Write a solution to find all customers who never order anything.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+
+
+ */
