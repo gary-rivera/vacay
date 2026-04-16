417bb8290ce540007116d20201329397438b391b
gary-rivera <a.gary.rivera@gmail.com>
2026-04-16 01:15:14 +0000

    Add solution reverse-string-prefix-1776302109578.ts

---
+++ solutions/reverse-string-prefix-1776302109578.ts
@@ -0,0 +1,57 @@
+function reverseFirstKChars(s: string, k: number): string {
+    let firstKChars = s.slice(0, k).split('').reverse().join('');
+    let remainingChars = s.slice(k);
+    return firstKChars + remainingChars;
+}
+
+/*
+question: You are given a string s and an integer k.
+
+Reverse the first k characters of s and return the resulting string.
+
+ 
+Example 1:
+
+
+Input: s = "abcd", k = 2
+
+Output: "bacd"
+
+Explanation:​​​​​​​
+
+The first k = 2 characters "ab" are reversed to "ba". The final resulting string is "bacd".
+
+
+Example 2:
+
+
+Input: s = "xyz", k = 3
+
+Output: "zyx"
+
+Explanation:
+
+The first k = 3 characters "xyz" are reversed to "zyx". The final resulting string is "zyx".
+
+
+Example 3:
+
+
+Input: s = "hey", k = 1
+
+Output: "hey"
+
+Explanation:
+
+The first k = 1 character "h" remains unchanged on reversal. The final resulting string is "hey".
+
+
+ 
+Constraints:
+
+
+	1 <= s.length <= 100
+	s consists of lowercase English letters.
+	1 <= k <= s.length
+
+ */
