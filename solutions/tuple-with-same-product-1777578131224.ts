28aa54adb96d068969598e0050d8e764c016cca5
gary-rivera <a.gary.rivera@gmail.com>
2026-04-30 19:42:19 +0000

    Add solution tuple-with-same-product-1777578131224.ts

---
+++ solutions/tuple-with-same-product-1777578131224.ts
@@ -0,0 +1,54 @@
+function countTuples(nums: number[]): number {
+    let count = 0;
+    const map = new Map<number, number[]>();
+    const len = nums.length;
+
+    for (let i = 0; i < len; i++) {
+        for (let j = i + 1; j < len; j++) {
+            const product = nums[i] * nums[j];
+            if (map.has(product)) {
+                count += map.get(product)!.length;
+            }
+            if (!map.has(product)) {
+                map.set(product, []);
+            }
+            map.get(product)!.push([i, j]);
+        }
+    }
+
+    return count * 4;
+}
+
+/*
+question: Given an array nums of distinct positive integers, return the number of tuples (a, b, c, d) such that a * b = c * d where a, b, c, and d are elements of nums, and a != b != c != d.
+
+ 
+Example 1:
+
+Input: nums = [2,3,4,6]
+Output: 8
+Explanation: There are 8 valid tuples:
+(2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3)
+(3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+
+
+Example 2:
+
+Input: nums = [1,2,4,5,10]
+Output: 16
+Explanation: There are 16 valid tuples:
+(1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2)
+(2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1)
+(2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4)
+(4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+
+
+ 
+Constraints:
+
+
+	1 <= nums.length <= 1000
+	1 <= nums[i] <= 104
+	All elements in nums are distinct.
+
+ */
