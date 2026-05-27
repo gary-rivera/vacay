2f0b7896a21ee8263ee9c82763632507135c72cc
gary-rivera <a.gary.rivera@gmail.com>
2026-05-27 21:02:10 +0000

    Add solution minimum-deletions-to-make-character-frequencies-unique-1779915719566.ts

---
+++ solutions/minimum-deletions-to-make-character-frequencies-unique-1779915719566.ts
@@ -0,0 +1,64 @@
+function minDeletions(s: string): number {
+    const freqMap: { [key: string]: number } = {};
+    for (let i = 0; i < s.length; i++) {
+        if (freqMap[s[i]]) {
+            freqMap[s[i]]++;
+        } else {
+            freqMap[s[i]] = 1;
+        }
+    }
+
+    const freqSet: Set<number> = new Set();
+    let deletions = 0;
+
+    for (let key in freqMap) {
+        while (freqSet.has(freqMap[key])) {
+            freqMap[key]--;
+            deletions++;
+        }
+        if (freqMap[key] > 0) {
+            freqSet.add(freqMap[key]);
+        }
+    }
+
+    return deletions;
+}
+
+/*
+question: A string s is called good if there are no two different characters in s that have the same frequency.
+
+Given a string s, return the minimum number of characters you need to delete to make s good.
+
+The frequency of a character in a string is the number of times it appears in the string. For example, in the string "aab", the frequency of 'a' is 2, while the frequency of 'b' is 1.
+
+ 
+Example 1:
+
+Input: s = "aab"
+Output: 0
+Explanation: s is already good.
+
+
+Example 2:
+
+Input: s = "aaabbbcc"
+Output: 2
+Explanation: You can delete two 'b's resulting in the good string "aaabcc".
+Another way it to delete one 'b' and one 'c' resulting in the good string "aaabbc".
+
+Example 3:
+
+Input: s = "ceabaacb"
+Output: 2
+Explanation: You can delete both 'c's resulting in the good string "eabaab".
+Note that we only care about characters that are still in the string at the end (i.e. frequency of 0 is ignored).
+
+
+ 
+Constraints:
+
+
+	1 <= s.length <= 105
+	s contains only lowercase English letters.
+
+ */
