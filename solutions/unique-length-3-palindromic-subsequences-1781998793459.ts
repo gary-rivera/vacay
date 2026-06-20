8a61140e54ba7e1fed1777882f9c58c7bc2bc7b4
gary-rivera <a.gary.rivera@gmail.com>
2026-06-20 23:39:59 +0000

    Add solution unique-length-3-palindromic-subsequences-1781998793459.ts

---
+++ solutions/unique-length-3-palindromic-subsequences-1781998793459.ts
@@ -0,0 +1,71 @@
+function countPalindromicSubsequence(s: string): number {
+    let result = 0;
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    const firstOccurrence = new Array(26).fill(Number.MAX_SAFE_INTEGER);
+    const lastOccurrence = new Array(26).fill(0);
+
+    for (let i = 0; i < s.length; i++) {
+        firstOccurrence[alphabet.indexOf(s[i])] = Math.min(firstOccurrence[alphabet.indexOf(s[i])], i);
+        lastOccurrence[alphabet.indexOf(s[i])] = i;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (firstOccurrence[i] < lastOccurrence[i]) {
+            const set = new Set(s.slice(firstOccurrence[i] + 1, lastOccurrence[i]));
+            result += set.size;
+        }
+    }
+
+    return result;
+}
+
+/*
+question: Given a string s, return the number of unique palindromes of length three that are a subsequence of s.
+
+Note that even if there are multiple ways to obtain the same subsequence, it is still only counted once.
+
+A palindrome is a string that reads the same forwards and backwards.
+
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+
+	For example, "ace" is a subsequence of "abcde".
+
+
+ 
+Example 1:
+
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+
+
+Example 2:
+
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+
+
+Example 3:
+
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+
+
+ 
+Constraints:
+
+
+	3 <= s.length <= 105
+	s consists of only lowercase English letters.
+
+ */
