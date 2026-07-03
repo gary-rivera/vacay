3d51129940d556bb6268cbe92a35778c37fcf559
gary-rivera <a.gary.rivera@gmail.com>
2026-07-03 21:28:52 +0000

    Add solution logical-or-of-two-binary-grids-represented-as-quad-trees-1783114116607.ts

---
+++ solutions/logical-or-of-two-binary-grids-represented-as-quad-trees-1783114116607.ts
@@ -0,0 +1,115 @@
+class Node {
+    val: boolean;
+    isLeaf: boolean;
+    topLeft: Node | null;
+    topRight: Node | null;
+    bottomLeft: Node | null;
+    bottomRight: Node | null;
+
+    constructor(val?: boolean, isLeaf?: boolean, topLeft?: Node, topRight?: Node, bottomLeft?: Node, bottomRight?: Node) {
+        this.val = (val === undefined ? false : val);
+        this.isLeaf = (isLeaf === undefined ? false : isLeaf);
+        this.topLeft = (topLeft === undefined ? null : topLeft);
+        this.topRight = (topRight === undefined ? null : topRight);
+        this.bottomLeft = (bottomLeft === undefined ? null : bottomLeft);
+        this.bottomRight = (bottomRight === undefined ? null : bottomRight);
+    }
+}
+
+function intersect(quadTree1: Node | null, quadTree2: Node | null): Node | null {
+    if (quadTree1 === null || quadTree2 === null) {
+        return null;
+    }
+
+    if (quadTree1.isLeaf) {
+        return quadTree1.val ? quadTree1 : quadTree2;
+    }
+
+    if (quadTree2.isLeaf) {
+        return quadTree2.val ? quadTree2 : quadTree1;
+    }
+
+    quadTree1.topLeft = intersect(quadTree1.topLeft, quadTree2.topLeft);
+    quadTree1.topRight = intersect(quadTree1.topRight, quadTree2.topRight);
+    quadTree1.bottomLeft = intersect(quadTree1.bottomLeft, quadTree2.bottomLeft);
+    quadTree1.bottomRight = intersect(quadTree1.bottomRight, quadTree2.bottomRight);
+
+    if (quadTree1.topLeft !== null && quadTree1.topLeft.isLeaf && quadTree1.topRight !== null && quadTree1.topRight.isLeaf && quadTree1.bottomLeft !== null && quadTree1.bottomLeft.isLeaf && quadTree1.bottomRight !== null && quadTree1.bottomRight.isLeaf && quadTree1.topLeft.val === quadTree1.topRight.val && quadTree1.topRight.val === quadTree1.bottomLeft.val && quadTree1.bottomLeft.val === quadTree1.bottomRight.val) {
+        quadTree1.isLeaf = true;
+        quadTree1.val = quadTree1.topLeft.val;
+    }
+
+    return quadTree1;
+}
+
+/*
+question: A Binary Matrix is a matrix in which all the elements are either 0 or 1.
+
+Given quadTree1 and quadTree2. quadTree1 represents a n * n binary matrix and quadTree2 represents another n * n binary matrix.
+
+Return a Quad-Tree representing the n * n binary matrix which is the result of logical bitwise OR of the two binary matrixes represented by quadTree1 and quadTree2.
+
+Notice that you can assign the value of a node to True or False when isLeaf is False, and both are accepted in the answer.
+
+A Quad-Tree is a tree data structure in which each internal node has exactly four children. Besides, each node has two attributes:
+
+
+	val: True if the node represents a grid of 1's or False if the node represents a grid of 0's.
+	isLeaf: True if the node is leaf node on the tree or False if the node has the four children.
+
+
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+}
+
+We can construct a Quad-Tree from a two-dimensional area using the following steps:
+
+
+	If the current grid has the same value (i.e all 1's or all 0's) set isLeaf True and set val to the value of the grid and set the four children to Null and stop.
+	If the current grid has different values, set isLeaf to False and set val to any value and divide the current grid into four sub-grids as shown in the photo.
+	Recurse for each of the children with the proper sub-grid.
+
+
+If you want to know more about the Quad-Tree, you can refer to the wiki.
+
+Quad-Tree format:
+
+The input/output represents the serialized format of a Quad-Tree using level order traversal, where null signifies a path terminator where no node exists below.
+
+It is very similar to the serialization of the binary tree. The only difference is that the node is represented as a list [isLeaf, val].
+
+If the value of isLeaf or val is True we represent it as 1 in the list [isLeaf, val] and if the value of isLeaf or val is False we represent it as 0.
+
+ 
+Example 1:
+ 
+Input: quadTree1 = [[0,1],[1,1],[1,1],[1,0],[1,0]]
+, quadTree2 = [[0,1],[1,1],[0,1],[1,1],[1,0],null,null,null,null,[1,0],[1,0],[1,1],[1,1]]
+Output: [[0,0],[1,1],[1,1],[1,1],[1,0]]
+Explanation: quadTree1 and quadTree2 are shown above. You can see the binary matrix which is represented by each Quad-Tree.
+If we apply logical bitwise OR on the two binary matrices we get the binary matrix below which is represented by the result Quad-Tree.
+Notice that the binary matrices shown are only for illustration, you don't have to construct the binary matrix to get the result tree.
+
+
+
+Example 2:
+
+Input: quadTree1 = [[1,0]], quadTree2 = [[1,0]]
+Output: [[1,0]]
+Explanation: Each tree represents a binary matrix of size 1*1. Each matrix contains only zero.
+The resulting matrix is of size 1*1 with also zero.
+
+
+ 
+Constraints:
+
+
+	quadTree1 and quadTree2 are both valid Quad-Trees each representing a n * n grid.
+	n == 2x where 0 <= x <= 9.
+
+ */
